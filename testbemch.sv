3a02e944d93019964459ce0ed789cdff6c7f72bf
khuang38 <alion13579@gmail.com>
2018-02-13 23:46:19 -0600

    MODIFIED testbench

--- testbemch.sv
+++ testbemch.sv
@@ -7,8 +7,8 @@ timeprecision 1ns;
 // These signals are internal because the processor will be
 // instantiated as a submodule in testbench.
 logic Clk = 0;
-logic Reset, ClearA_LoadB, Run;
-logic [7:0] S;
+logic Reset, ClearA_LoadB, Execute;
+logic [7:0] SW;
 logic X;
 logic [7:0] Aval,
 		 Bval;
@@ -40,39 +40,39 @@ end
 initial begin: TEST_VECTORS
 Reset = 0;		// Toggle Rest
 ClearA_LoadB = 1;
-Run = 1;
-S = 8'b11000101;	// Specify S
+Execute = 1;
+SW = 8'b11000101;	// Specify SW
 
 #2 Reset = 1;
 
 #2 ClearA_LoadB = 0;	// Toggle LoadB
 #2 ClearA_LoadB = 1;
 
-#2 S = 8'b00000111;	// now the multiplier is 00000111
+#2 SW = 8'b00000111;	// now the multiplier is 00000111
 
-#2 Run = 0;	// begin our first calculation -59*7
+#2 Execute = 0;	// begin our first calculation -59*7
 
-#40 Run = 1; // togle Run
+#40 Execute = 1; // togle Execute
 
 #2 ClearA_LoadB = 0;	// Toggle LoadB
 #2 ClearA_LoadB = 1; //now register B becomes 00000111
-#2 S = 8'b11000101; //now the multiplier is 11000101
-#2 Run = 0;//compute 7*-59
-#40 Run = 1; // togle Run
+#2 SW = 8'b11000101; //now the multiplier is 11000101
+#2 Execute = 0;//compute 7*-59
+#40 Execute = 1; // togle Execute
 
 #2 ClearA_LoadB = 0;	// Toggle LoadB
 #2 ClearA_LoadB = 1; // now the register B becomes 11000101
-#2 S = 8'b11111001; // now the multiplier is -7
+#2 SW = 8'b11111001; // now the multiplier is -7
 
-#2 Run = 0;	// begin our first calculation -59*7
-#40 Run = 1; // togle Run
+#2 Execute = 0;	// begin our first calculation -59*7
+#40 Execute = 1; // togle Execute
 
-#32 S = 8'00000111;
+#32 SW = 8'00000111;
 #2 ClearA_LoadB = 0;	// Toggle LoadB
 #2 ClearA_LoadB = 1; // now the register B becomes 00000111
-#2 S = 8'00111011; // NOW THE multiplier is 59
-#2 RUN = 0; // we now start the second computation:7*59
-#9 Run =1;
+#2 SW = 8'00111011; // NOW THE multiplier is 59
+#2 Execute = 0; // we now start the second computation:7*59
+#9 Execute =1;
 
 
 
